44463a74b678ae24029bc1bb010ccc31e14e9601
刘超凡 <lcfme@sina.com>
2018-03-21 00:34:14 +0800

    fixbug StateMachine.js

--- StateMachine.js
+++ StateMachine.js
@@ -1,20 +1,22 @@
 function StateMachine(fn) {
     function P(fn, { _value, _status = 'pending' } = {}) {
         var callback;
+        var temp = null;
         function then(f) {
             callback = f;
             if (o._status === 'ok') {
                 return P(null, { _value: callback(o._value), _status: 'ok' });
             } else {
-                return o;
+                temp = P(null, { _value: o._value, _status: 'pending' });
+                return temp;
             }
         }
         function resolve(_v) {
             o._status = 'ok';
             o._value = _v;
             if (typeof callback === 'function') {
-                return P(null, { _value: callback(o._value), _status: 'ok' });
-            } else {
+                temp._value = callback(temp._value);
+                temp._status = 'ok';
             }
         }
         var o = {
